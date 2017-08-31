7c519e5ae8cf804d9d19def49b66a8216be7c938
LiuYao <liuyao_public@163.com>
2017-08-31 20:50:36 +0800

    2017-8-31 LogisticRegression

--- marchine_learning/linear_model/LogisticRegression.py
+++ marchine_learning/linear_model/LogisticRegression.py
@@ -1,6 +1,9 @@
 #encoding=utf-8
 '''
-Define the LogisticRegression class
+Copyright : CNIC
+Author : LiuYao
+Date : 2017-8-31
+Description : Define the LogisticRegression class
 '''
 
 import numpy as np
@@ -10,34 +13,65 @@ class LogisticRegression(object):
     implement the lr relative functions
     '''
 
-    def __init__(self, alpha=0.1, iterator_num=100):
+    def __init__(self, alpha=0.1, iterator_num=100, optimization='sgd'):
+        '''
+        lr parameters init
+        Args:
+            alpha: the learning rate, default is 0.1.
+            iterator_num: the count of iteration, default is 100.
+            optimization: the optimization method, such as 'sgd', 'gd', default is 'sgd'.
+        '''
         self.alpha = alpha
         self.iterator_num = iterator_num
+        self.optimization = optimization
 
     def train(self, x_train, y_train):
         '''
         lr train function
         Args:
-            x_train: the train data
-            y_train: the train labels
+            x_train: the train data, shape = (m, n), m is the count of the samples, 
+                     n is the count of the features
+            y_train: the train labels, shape = (m, 1), m is the count of the samples
         '''
         m, n = x_train.shape
         x_train = np.mat(x_train)
         self.theta = np.mat(np.random.rand(n + 1, 1))
         x_train = np.hstack((x_train, np.ones((m, 1))))
-        y_train = np.mat(y_train)
-        self.gradient_decent(x_train, y_train)
-        return self.theta
+        y_train = np.mat(np.reshape(y_train, (m, 1)))
+        if(self.optimization == 'gd'):
+            self.__gradient_decent__(x_train, y_train)
+        elif(self.optimization == 'sgd'):
+            self.__stochastic_gradient_decent__(x_train, y_train)
 
     def sigmoid(self, x):
-        return 1 / (1 + np.exp(np.mat(x)))
+        return 1.0 / (1.0 + np.exp(-x))
 
-    def gradient_decent(self, x_train, y_train):
+    def __gradient_decent__(self, x_train, y_train):
+        [m, n] = x_train.shape
         for i in xrange(self.iterator_num):
             print "step : %d" % i
-            self.theta = self.theta - self.alpha * np.transpose(x_train) * (self.sigmoid(x_train * self.theta) - y_train)
+            self.theta = self.theta - 1.0 / m * self.alpha * np.transpose(x_train) * (self.sigmoid(x_train * self.theta) - y_train)
     
+    def __stochastic_gradient_decent__(self, x_train, y_train):
+        [m, n] = x_train.shape
+        for j in xrange(self.iterator_num):
+            data_index = range(m)
+            print "step : ", j
+            for i in xrange(m):
+                #动态调整学习率
+                self.alpha = 4 / (1.0 + j + i) + 0.01
+                #随机选取一个样本，随后将其从dataIndex中删除
+                rand_index = int(np.random.uniform(0, len(data_index)))
+                error = self.sigmoid(np.dot(x_train[rand_index, :], self.theta)) - y_train[rand_index]
+                self.theta = self.theta - np.multiply(self.alpha, np.multiply(error, x_train[rand_index].T))
+
+
     def predict(self, x_test):
+        '''
+        lr predict function
+        Args:
+            x_test: the test data, shape = (m, 1), m is the count of the test data
+        '''
         [m, n] = x_test.shape
         x_test = np.mat(x_test)
         x_test = np.hstack((x_test, np.ones((m, 1))))
---
+++ test.py
@@ -0,0 +1,54 @@
+#encoding=utf-8
+'''
+Copyright : CNIC
+Author : LiuYao
+Date : 2017-8-31
+Description : test my algorithm
+'''
+
+import pandas as pd
+import numpy as np
+from marchine_learning.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+def load_data():
+    '''
+    load data
+    '''
+    data = pd.read_csv('./data.csv')
+    x = data[['x', 'y']]
+    y = data['label']
+    return x, y
+
+def plot(x_train, y_train, theta):
+        [m, n] = x_train.shape
+        plt.scatter(x_train.values[:, 0], x_train.values[:, 1], c=y_train)
+        x1 = np.random.rand(100, 1) * 25
+        x2 = (-theta[2] - x1 * theta[0]) / theta[1]
+        plt.plot(x1, x2)
+        plt.show()
+
+def train():
+    x, y = load_data()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+    lr = LogisticRegression.LogisticRegression(iterator_num=100)
+    lr.train(x_train.values, y_train.values.T)
+    y_predict = lr.predict(x_test.values)
+    y_predict[y_predict > 0.5] = 1
+    y_predict[y_predict < 0.5] = 0
+    print lr.theta
+    print "accuracy : ", np.sum(y_predict.getA()[0] == y_test.values) / (len(y_test) * 1.0)
+
+def main():
+    '''
+    program entry
+    '''
+    x, y = load_data()
+    lr = LogisticRegression.LogisticRegression(iterator_num=5, optimization='sgd')
+    lr.train(x.values, y.values.T)
+    print lr.theta
+    plot(x, y, lr.theta)
+    # train()
+
+if __name__ == '__main__':
+    main()
